fa7306e1970bc9fd5eba0a8e5314d5171afc18e8
Zaccaria Ziani <Zaccaria Ziani>
2020-05-05 23:58:47 +0200

    modif jour01/job03/index.php

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -5,14 +5,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="index.css" rel="stylesheet">
+    <title>Media Queries</title>
 </head>
 <body>
-<main class="container">
-		<section class="section"></section>
-		<section class="section"></section>
-		<section class="section"></section>
-		<section class="section"></section>
-	</main>
+    <div id="container">
+    <section class="section"></section>
+    <section class="section"></section>
+    <section class="section"></section>
+    <section class="section"></section>
+    </div>
 </body>
 </html>
